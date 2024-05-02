8c929c9de67c78103be1d4a9f8c1cdcf0d604a4d
Mithat Ozgun <mithatozgun@hotmail.com>
2024-05-02 10:15:58 +0200

    wip

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
-# from flask_cors import CORS
+import os
+
+# Set the cache directory
+os.environ['TRANSFORMERS_CACHE'] = '/tmp/transformers_cache/'
 
 app = Flask(__name__)
 # CORS(app, resources={r"/*": {"origins": "*"}}) 
@@ -12,15 +15,15 @@ sentiment_analyzer = pipeline("sentiment-analysis")
 def home():
     return 'Hello, World!'
 
-# @app.route('/analyze', methods=['POST'])
-# def analyze():
-#     print(request.json)
-#     data = request.json
-#     text = data['text']
-#     result = sentiment_analyzer(text)
-#     response = jsonify(result)
-#     # response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    print(request.json)
+    data = request.json
+    text = data['text']
+    result = sentiment_analyzer(text)
+    response = jsonify(result)
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
