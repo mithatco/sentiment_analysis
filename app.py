8c4df98a472a17f7f0d0906307eb558eb53af475
Mithat Ozgun <mithatozgun@hotmail.com>
2024-05-02 10:03:20 +0200

    wip

--- app.py
+++ app.py
@@ -2,27 +2,25 @@ from flask import Flask, request, jsonify
 from transformers import pipeline
 # from flask_cors import CORS
 
-print()
-
 app = Flask(__name__)
 # CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 # Load the sentiment analysis pipeline
-sentiment_analyzer = pipeline("sentiment-analysis")
+# sentiment_analyzer = pipeline("sentiment-analysis")
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    print(request.json)
-    data = request.json
-    text = data['text']
-    result = sentiment_analyzer(text)
-    response = jsonify(result)
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+# @app.route('/analyze', methods=['POST'])
+# def analyze():
+#     print(request.json)
+#     data = request.json
+#     text = data['text']
+#     result = sentiment_analyzer(text)
+#     response = jsonify(result)
+#     # response.headers.add('Access-Control-Allow-Origin', '*')
+#     return response
 
 if __name__ == '__main__':
     app.run(debug=True)
